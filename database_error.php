84bb72a7df194e001811486896159b4bb9687ec5
lianna200 <lianna200@gmail.com>
2017-08-11 22:03:19 -0400

    Update database_error.php

--- database_error.php
+++ database_error.php
@@ -1,9 +1,27 @@
-<?php  ?>
-<div id="main">
-    <h1>Database Error</h1>
-    <p class="first_paragraph">There was an error connecting to the database.</p>
-    <p>The database must be installed as described in the appendix.</p>
-    <p>MySQL must be running as described in chapter 1.</p>
-    <p class="last_paragraph">Error message: <?php echo $error_message; ?></p>
-</div><!-- end main -->
-<?php  ?>
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>TEst</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+    <header><h1>TEst</h1></header>
+
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed as described in the appendix.</p>
+        <p>MySQL must be running as described in chapter 1.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+        <p>&nbsp;</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> test, Inc.</p>
+    </footer>
+</body>
+</html>
