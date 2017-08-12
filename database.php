1d4340c7f954838968d1f6b3c3172ec3bf9638e2
lianna200 <lianna200@gmail.com>
2017-08-11 20:14:27 -0400

    Update database.php

--- database.php
+++ database.php
@@ -22,3 +22,12 @@ class Database {
     }
 }
 ?>
+
+<?php
+$con=mysqli_connect("localhost","root","","todo_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
