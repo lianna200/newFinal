421799d801a6fac167d833931f6ae99accad8827
lianna200 <lianna200@gmail.com>
2017-08-11 22:00:56 -0400

    Update database.php

--- database.php
+++ database.php
@@ -3,10 +3,12 @@
     $dsn = 'mysql:dbname=al224;host=sql1.njit.edu';
     $username = 'al224';
     $password = 'wSt5Tjc0';
+
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-        include('../database_error.php');
+        include('database_error.php');
         exit();
     }
+?>
