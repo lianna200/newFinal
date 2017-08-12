5eb2f896a5c33ecdb2777e303b82a95bed13a3f1
lianna200 <lianna200@gmail.com>
2017-08-11 21:09:32 -0400

    Update database_error.php

--- database_error.php
+++ database_error.php
@@ -1,4 +1,4 @@
-<?php include '../view/header.php'; ?>
+<?php  ?>
 <div id="main">
     <h1>Database Error</h1>
     <p class="first_paragraph">There was an error connecting to the database.</p>
@@ -6,4 +6,4 @@
     <p>MySQL must be running as described in chapter 1.</p>
     <p class="last_paragraph">Error message: <?php echo $error_message; ?></p>
 </div><!-- end main -->
-<?php include '../view/footer.php'; ?>
+<?php  ?>
