0f400aa7c6724bb4a9f2236bba737c34de402a2e
lianna200 <lianna200@gmail.com>
2017-08-11 20:15:51 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,33 +1,13 @@
-<?php
-class Database {
-    private static $dsn = 'mysql:dbname=al224;host=sql1.njit.edu';
-    private static $username = 'al224';
-    private static $password = 'wSt5Tjc0';
-    private static $db;
-    
-    private function __construct() {}
-    public static function getDB () {
-        if (!isset(self::$db)) {
-            try {
-                self::$db = new PDO(self::$dsn,
-                                     self::$username,
-                                     self::$password);
-            } catch (PDOException $e) {
-                $error_message = $e->getMessage();
-                include('../errors/database_error.php');
-                exit();
-            }
-        }
-        return self::$db;
+13 lines (12 sloc)  328 Bytes
+<?p
+    $dsn = 'mysql:dbname=al224;host=sql1.njit.edu';
+    $username = 'al224';
+    $password = 'wSt5Tjc0';
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../database_error.php');
+        exit();
     }
-}
-?>
-
-<?php
-$con=mysqli_connect("localhost","root","","todo_db");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 ?>
